b4f588e3638a432fed5cc4c9c9b70d24a46aee14
tasi <daunte_west1@baylor.edu>
2023-01-30 14:10:52 -0600

    in class lecture

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,9 +1,22 @@
 import random
 
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
+phonebook = {}
+# key    #values
+phonebook = {'Chris': '555−1111',
+             'Katie': '555−2222',
+             'Joanne': '555−3333'}  # dictionaires have scriggly brackets, key is always on left, value is always on right
+# values can be any object (string, int, float, list, tuple, dict)
+# a list needs an index value a dictionary needs a key value
+'''
+mydictionary = dict(m=8, n=9) #a way to create a dictionary
+print(mydictionary)
 
+print(f"Number of key value pairs")
+
+print()
+
+
+print()
 
 
 print()
@@ -11,24 +24,27 @@ print('*****  start section 1 - print dictionary ********')
 print()
 
 
-
-
-
 print()
 print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = 'Chris'
+
+if name in phonebook:
+    print(phonebook[name]))
+else:
+    print([name]'is not in the phonebook')
 
 
-
+print(phonebook['Chris']) #prints Chris's phone number (a key error means there is no match) (make sure case is correct)
 
 
 
@@ -39,15 +55,16 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
+phonebook['Chris'] = '555-4444'
+# keys in dictionaires are unique you can't have duplicate keys
+phonebook['Joe'] = '555-0123'
 
-
+print(phonebook)
 
 
 print()
@@ -56,14 +73,13 @@ print()
 
 
 
-
-
-
 print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+print(phonebook)
+del phonebook['Chris']
+print(phonebook)
 
 
 print()
@@ -73,16 +89,21 @@ print()
 
 
 
-
-
 print()
 print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is: {phonebook[key]}")
 
+for value in phonebook.values():
+    print(f"The value is: {value}")
 
+for k, v in phonebook.items():
+    print((f"The key is: {k} and the value is: {v}"))
 
-
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
 print()
 print('*****  end section 5 ********')
@@ -96,10 +117,15 @@ print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+name = 'chris'
+phone = phonebook.get(name, "key not found")
+
+print(phone)
 
 
+phonebook.clear()  # clears out all elements in the object
 
-
+print(phonebook)
 
 print()
 print('*****  end section 6 ********')
@@ -112,8 +138,9 @@ print('*****  start section 7 - using pop method ********')
 print()
 
 
-
-
+remove = phonebook.pop('Chris', 'not found')
+print(remove)
+print(phonebook) #chris is no longer in the dictionary
 
 
 print()
@@ -127,32 +154,33 @@ print('*****  start section 8 - using popitem ********')
 print()
 
 
+a = phonebook.popitem() #not random it keeps popping the last element
 
 
+print(a)
+
+print(phonebook)
 
 
 print()
 print('*****  end section 8 ********')
 print()
 
-
+'''''''''''
 
 print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
+list_of_keys = list(phonebook)
+random_key = random.choice(list_of_keys)
+print(random_key)  # this works better for randomization
+print(phonebook[random_key])
 
-
+# try one line of code using no variables to do above code
+print(phonebook[random.choice(list(phonebook))])
 
 
 print()
 print('*****  end section 9 ********')
 print()
-
-
-'''
-
-
-
-
-
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,22 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+
+# name of the second child
+print(person.get("children")[1])
+print(person["children"][1])  # easiest way to get the second child
+
+# name of the cat
+print(person["pets"]["cat"])
+
+# use a for loop to list all the children
+for i in person["children"]:
+    print(i)
+
+# print out the pets in this format;
+# 'Type of pet: dog', 'Name of pet: Fido'
+for i, j in person['pets'].items():
+    print(f"Type of pet: {i} name of pet: {j}")
